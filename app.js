d33ff43a38cc58e4854d49b2d0457d2bc3bdd432
Afzalkhon <00009252wiut@gmail.com>
2021-04-05 12:12:03 +0500

    Third commit

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
+/* ---- Imports ---- */
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+
 app.set('view engine','pug')
 
+/* ---- Middle whereas ---- */
 app.use('/static',express.static('public'))
-
+app.use(express.urlencoded({ extended: false }))
 
 /* ----- localhost:3000 ----- */ 
 app.get('/', (req,res) => {
     res.render('home')
 })
 
+/* ---- the function serves create button ---- */
 app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+/* ---- Submission button to back-end ---- */
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+
+    if(title.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/notes.json', (err, data) => {
+            if(err) throw err
+
+            const notes = JSON.parse(data)
+
+            notes.push({
+                title: title,
+                description: description,
+            })
+        })
+    }
+
     res.render('create')
 })
 
@@ -19,6 +47,10 @@ const notes = ['Some awesome titles', 'Some awesome titles 2']
 
 app.get('/notes', (req, res) => {
     res.render('notes', { notes: notes })
+})
+
+app.get('/notes/detail', (req, res) => {
+    res.render('detail')
 })
 
 app.listen(3000, err => {
